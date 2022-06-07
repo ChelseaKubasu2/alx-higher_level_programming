b79eb5801eba0b211b4b68a6e73f7791e4ce489d
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2022-06-07 01:53:49 +0000

    fix task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    Adds elements of 2 tuples
+    Args:
+        tuple_a: tuple
+        tuple_b: tuple
+    Return:
+        Tuple containing addition result
+    """
+    a_0 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    a_1 = tuple_a[1] if len(tuple_a) >= 2 else 0
 
-    a = len(tuple_a)
-    b = len(tuple_b)
+    b_0 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    b_1 = tuple_b[1] if len(tuple_b) >= 2 else 0
 
-    sums = ((tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0),
-            (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0))
-
-    return 
+    return (a_0 + b_0, a_1 + b_1)
