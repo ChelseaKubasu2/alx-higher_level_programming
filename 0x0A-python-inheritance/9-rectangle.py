b14bb4c5ef0c2403ea092c3b6a4e3ddfd8440022
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2022-07-05 01:01:28 +0000

    add file

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
-"""
-Module 9-rectangle
-Contains parent class BaseGeometry
-with public instance method area and integer_validation
-Contains subclass Rectangle
-with instantiation of private attributes width and height, validated by parent,
-extends parent's area method and prints with __str__
-"""
-
+"""9-base_geometry.py"""
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """inherits from BaseGeometry
-    Methods:
-        __init__(self, width, height)
-        area(self)
-        __str__
-    """
+    """Implements a Rectangle geometric shape"""
+
     def __init__(self, width, height):
-        """validate and initialize width and height
-        Args:
-            width (int): private
-            height (int): private
-        """
+        """Initializes width and height attributes of Rectangle object"""
+
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """extends parent's empty method and returns area"""
+        """Calculates the area of a Rectangle object"""
         return self.__width * self.__height
 
     def __str__(self):
-        """prints [Rectangle] <width>/<height>"""
-        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
-                                         self.__width, self.__height
+        """String representation of a Rectangle object"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
