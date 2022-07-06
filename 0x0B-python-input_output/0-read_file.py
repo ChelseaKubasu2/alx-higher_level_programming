c6c6ecee80e1fc7e80e407ab10ef00540030d309
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2022-07-06 01:02:04 +0000

    add file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""0-read_file.py"""
+"""
+Module 0-read_file
+Contains function that reads and prints contents from file
+"""
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout
-    Args:
-        filename: text file to read
-    Returns: None
-    """
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+    """Read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
